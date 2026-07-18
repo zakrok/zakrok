8edcb19909dedcff5f9c316b17eb8263d9ea942b
ecstasy <31936449+zakrok@users.noreply.github.com>
2026-07-18 20:00:32 +0200

    fix: random WebStorm rewriting of / to /static back to /

--- zakrok.dev/src/routes/blog/+page.svelte
+++ zakrok.dev/src/routes/blog/+page.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <nav class="font-pixel text-white w-1/2 mx-auto mb-12 mt-6">
-    <a href="/static" class="underline">home</a>
+    <a href="/" class="underline">home</a>
     <a href="/projects" class="underline">projects</a>
     <a href="/blog" class="underline bg-white text-black">blog</a>
     <a href="/about" class="underline">about</a>
